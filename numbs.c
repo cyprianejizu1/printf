38ba6216b10e1436f57b78bb51b87b44db0d1d55
apholabby <afolabiadebola236@gmail.com>
2022-10-20 17:29:48 -0700

    checking file function

--- numbs.c
+++ numbs.c
@@ -13,7 +13,7 @@ char *convert(long int num, int base, int flags, params_t *params)
 {
 	static char *array;
 	static char buffer[50];
-	char sign = 0;
+	char sign = 0
 	char *ptr;
 	unsigned long n = num;
 	(void)params;
